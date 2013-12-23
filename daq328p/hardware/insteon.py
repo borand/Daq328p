8691f4aa872531c411815e19f0d590c66013cbfc
satellite <borand@borowiec.ca>
2013-12-23 15:14:07 -0500

    wip

--- daq328p/hardware/insteon.py
+++ daq328p/hardware/insteon.py
@@ -280,14 +280,14 @@ if __name__ == '__main__':
     plm = InsteonPLM()
     plm.log.level = 50
 
-
-    timestamp = datetime.datetime.now()
-    val = []
-    for device in all_devices:
-        
-        tmp  = plm.GetSwitchStatus(device)
-        if tmp is not None:
-             val.append([0, hex2str(device), tmp])
-    print val
-    print submit(val,submit_to="192.168.1.133",port=8000,timestamp='')
+    while 1:
+        timestamp = datetime.datetime.now()
+        val = []
+        for device in all_devices:
+            
+            tmp  = plm.GetSwitchStatus(device)
+            if tmp is not None:
+                 val.append([0, hex2str(device), tmp])
+        print val
+        print submit(val,submit_to="192.168.1.133",port=8000,timestamp='')
     plm.stop()
